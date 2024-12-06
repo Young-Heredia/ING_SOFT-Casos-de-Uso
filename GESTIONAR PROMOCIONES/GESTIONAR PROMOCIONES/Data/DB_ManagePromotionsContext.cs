7b26b9c3015521d6f4edcd662a2a764c40f2c7a7
Young-Heredia <hse1010674@est.univalle.edu>
2024-12-06 02:20:08 -0400

    Versión 5.1 - PLANTILLA INICIAL DEL PROYECTO

---
+++ GESTIONAR PROMOCIONES/GESTIONAR PROMOCIONES/Data/DB_ManagePromotionsContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GESTIONAR_PROMOCIONES.Data
+{
+    public class DB_ManagePromotionsContext:DbContext
+    {
+        public DB_ManagePromotionsContext(DbContextOptions<DB_ManagePromotionsContext> options) : base(options) { }
+    }
+}
---
+++ GESTIONAR PROMOCIONES/GESTIONAR PROMOCIONES/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GESTIONAR_PROMOCIONES.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
